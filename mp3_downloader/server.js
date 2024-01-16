428f9858067ad051c563be48ec63f85996d4e2e5
Kermit3T <johntong0905@gmail.com>
2024-01-15 20:44:06 -0800

    modify server

--- mp3_downloader/server.js
+++ mp3_downloader/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
+const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/download', async (req, res) => {
